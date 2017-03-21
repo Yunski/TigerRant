d005e33b7fa16abe022c149f6777d85ef201b63b
Yun Teng <mindtracker.yun24@gmail.com>
2017-03-20 23:33:16 -0400

    add course evaluation scraper and update database and views, remove util.py

--- util.py
+++
@@ -1,8 +0,0 @@
-import html
-
-def unescape(s):
-    s = html.unescape(s)
-
-if __name__ == '__main__':
-    s = unescape("Victor Hugo's &amp;lt;i&amp;gt;Les Misérables&amp;lt;/i&amp;gt;");
-    print(s)
